8e46a05e6cb52426d93ac42e0185c16a1931b125
Adrian de Freitas <Adefreit@users.noreply.github.com>
2022-07-17 12:34:33 -0600

    Uploading Template

---
+++ quiz08functs.h
@@ -0,0 +1,18 @@
+/** quiz08functs.h
+* ===========================================================
+*    Name: 
+* Section:
+* Purpose: Quiz 8
+* ===========================================================
+*/
+
+#ifndef QUIZ08FUNCTS_H
+#define QUIZ08FUNCTS_H
+
+// YOUR CODE GOES HERE
+
+
+// This function is being provided to you.  Do Not Touch!
+int loadCarInventory(Car* carInventory, int numToRead);
+
+#endif
